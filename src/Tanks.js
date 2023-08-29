e04c22a7a09d0c48192c6440a7e0fa5b63a50219
huzjm <huzefa.juzar@gmail.com>
2023-08-30 04:24:41 +0500

    Fifth Update

--- src/Tanks.js
+++ src/Tanks.js
@@ -3,14 +3,14 @@ import SubMenuTemplate from './SubMenuTemplate';
 
 const Tanks = () => {
   return (
-    <SubMenuTemplate
+    <div className='content'>  <SubMenuTemplate
     subPath='/storage/tanks'
     mainPath='/storage'
       mainHeading="Storage Tanks"
       submenuTitle="Tanks"
       image='/image/storage1.jpg'
       content="Our tanks are meticulously engineered to accommodate various liquids and volumes, ensuring safe storage in a range of industries. With a commitment to durability and precision, HES delivers tanks that meet regulatory standards while providing seamless integration into your operations."
-    />
+    /></div>
   );
 };
 
